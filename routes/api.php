e82579b268f20a8b6b9a5fc3a82e56ccfbdfdd0d
Elan Wittevrongel <elan.wittevrongel@gmail.com>
2023-06-14 15:01:16 +0200

    changes

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::group(['middleware' => ['scribe_routes']], function () {
+//     Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//         return $request->user();
+//     });
+
+//     Route::get('/fighters', 'FighterController@index');
+//     Route::post('/fighters', 'FighterController@store');
+//     Route::get('/fighters/{id}', 'FighterController@show');
+//     Route::put('/fighters/{id}', 'FighterController@update');
+//     Route::delete('/fighters/{id}', 'FighterController@destroy');
+// });
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
